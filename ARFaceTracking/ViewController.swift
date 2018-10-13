379a985741a20fe1cff17d35cd00936a737e23cf
黃胤璽 <eye999999999@gmail.com>
2018-10-13 22:39:39 +0800

    Initial Commit

---
+++ ARFaceTracking/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ARFaceTracking
+//
+//  Created by Phineas on 2018/10/13.
+//  Copyright © 2018 Phineas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
