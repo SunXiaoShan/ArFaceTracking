1e960e0af839becabb28ae7bd537ed509eb013f0
Phineas <sunxiaoshan@Phineasde-MacBook-Pro.local>
2018-10-13 23:30:22 +0800

    implement ar face

--- ARFaceTracking/ViewController.swift
+++ ARFaceTracking/ViewController.swift
@@ -7,14 +7,103 @@
 //
 
 import UIKit
+import ARKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var sceneView: ARSCNView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        guard ARFaceTrackingConfiguration.isSupported else { fatalError() }
+        sceneView.delegate = self
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        let configuration = ARFaceTrackingConfiguration()
+        
+        sceneView.session.run(configuration)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        sceneView.session.pause()
+    }
+    
+    func updateFeatures(for node: SCNNode, using anchor: ARFaceAnchor) {
+        for (feature, indices) in zip(features, featureIndices) {
+            let child = node.childNode(withName: feature, recursively: false) as? EmojiNode
+            let vertices = indices.map { anchor.geometry.vertices[$0] }
+            child?.updatePosition(for: vertices)
+            
+            switch feature {
+            case "leftEye":
+                let scaleX = child?.scale.x ?? 1.0
+                let eyeBlinkValue = anchor.blendShapes[.eyeBlinkLeft]?.floatValue ?? 0.0
+                child?.scale = SCNVector3(scaleX, 1.0 - eyeBlinkValue, 1.0)
+                
+                let eyeLookDL = anchor.blendShapes[.eyeLookDownLeft]?.floatValue ?? 0.0
+                if (eyeLookDL > 0.4) {
+                    let messageVC = UIAlertController(title: "Message Title", message: "Account Created successfully" , preferredStyle: .actionSheet)
+                    present(messageVC, animated: true) {
+                        Timer.scheduledTimer(withTimeInterval: 0.5, repeats: false, block: { (_) in
+                            messageVC.dismiss(animated: true, completion: nil)})}
+                }
+                print("phineas - eyeLookDL:\(eyeLookDL)")
+                
+                
+            case "rightEye":
+                let scaleX = child?.scale.x ?? 1.0
+                let eyeBlinkValue = anchor.blendShapes[.eyeBlinkRight]?.floatValue ?? 0.0
+                child?.scale = SCNVector3(scaleX, 1.0 - eyeBlinkValue, 1.0)
+                
+            case "mouth":
+                let jawOpenValue = anchor.blendShapes[.jawOpen]?.floatValue ?? 0.2
+                child?.scale = SCNVector3(1.0, 0.8 + jawOpenValue, 1.0)
+                
+            default:
+                break
+            }
+        }
+    }
+    
+    @IBAction func handleTap(_ sender: UITapGestureRecognizer) {
+        let location = sender.location(in: sceneView)
+        let results = sceneView.hitTest(location, options: nil)
+        for result in results {
+            if let node: EmojiNode = result.node as? EmojiNode {
+                node.next()
+            }
+        }
     }
 
 
+}
+
+extension ViewController: ARSCNViewDelegate {
+    
+    func renderer(_ renderer: SCNSceneRenderer, nodeFor anchor: ARAnchor) -> SCNNode? {
+        guard let faceAnchor = anchor as? ARFaceAnchor,
+            let device = sceneView.device else { return nil }
+        let faceGeometry = ARSCNFaceGeometry(device: device)
+        let node = SCNNode(geometry: faceGeometry)
+        node.geometry?.firstMaterial?.fillMode = .lines
+        
+        DispatchQueue.main.async {
+            self.updateFeatures(for: node, using: faceAnchor)
+        }
+        
+        return node
+    }
+    func renderer(_ renderer: SCNSceneRenderer, didUpdate node: SCNNode, for anchor: ARAnchor) {
+        guard let faceAnchor = anchor as? ARFaceAnchor, let faceGeometry = node.geometry as? ARSCNFaceGeometry else { return }
+        
+        faceGeometry.update(from: faceAnchor.geometry)
+        updateFeatures(for: node, using: faceAnchor)
+    }
 }
 
